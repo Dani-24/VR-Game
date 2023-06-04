284cac4275a9349a62995fa8f8f153c11f35b178
Dani-24 <79161140+Dani-24@users.noreply.github.com>
2023-06-04 20:21:03 +0200

    UI y poder salir del juego y tal

---
+++ VR Room Project/Assets/Scripts/MovementRecognizer.cs
@@ -0,0 +1,140 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+using PDollarGestureRecognizer;
+using System.IO;
+using UnityEngine.Events;
+
+public class MovementRecognizer : MonoBehaviour
+{
+    public XRNode inputSource;
+    public InputHelpers.Button inputButton;
+    public float inputThreshold = 0.1f;
+    public Transform movementSource;
+
+    public float newPositionThresholdDistance = 0.05f;
+    public GameObject drawingObjectPrefab;
+
+    public bool creationMode = true;
+    public string newGestureName;
+    private List<Gesture> trainingSet = new List<Gesture>();
+
+    public float recognitionThreshold = 0.9f;
+
+    [System.Serializable]
+    public class UnityStringEvent : UnityEvent<string> { }
+    public UnityStringEvent OnRecognized;
+
+    private bool isMoving = false;
+    private List<Vector3> positionList = new List<Vector3>();
+
+    public TextAsset canvasCode;
+
+    private void Start()
+    {
+        //if (!Application.isEditor)
+        //{
+        //    // BUILD
+        //    TextAsset xmlAsset = Resources.Load<TextAsset>("o.xml");
+        //    trainingSet.Add(GestureIO.ReadGestureFromFile(xmlAsset.ToString()));
+        //}
+        //{
+        //    // NOT BUILD
+        //    string[] gestureFiles = Directory.GetFiles(Application.persistentDataPath, "*.xml");
+
+        //    foreach (var item in gestureFiles)
+        //    {
+        //        trainingSet.Add(GestureIO.ReadGestureFromFile(item));
+        //    }
+        //}
+
+        trainingSet.Add(GestureIO.ReadGestureFromXML(canvasCode.ToString()));
+    }
+
+    private void Update()
+    {
+        InputHelpers.IsPressed(InputDevices.GetDeviceAtXRNode(inputSource), inputButton, out bool isPressed, inputThreshold);
+
+        if(!isMoving && isPressed)
+        {
+            StartMovement();
+        }
+        else if(isMoving && !isPressed)
+        {
+            EndMovement();
+        }
+        else if(isMoving && isPressed)
+        {
+            UpdateMovement();
+        }
+    }
+
+    void StartMovement()
+    {
+        Debug.Log("Start movement");
+        isMoving = true;
+
+        positionList.Clear();
+        positionList.Add(movementSource.position);
+
+        if (drawingObjectPrefab)
+        {
+            Destroy(Instantiate(drawingObjectPrefab, movementSource.position, Quaternion.identity),2f);
+        }
+    }
+
+    void EndMovement()
+    {
+        Debug.Log("End movement");
+        isMoving = false;
+
+        Point[] pointArray = new Point[positionList.Count];
+
+        for(int i = 0; i< positionList.Count; i++)
+        {
+            Vector2 screenPoint = Camera.main.WorldToScreenPoint(positionList[i]);
+            pointArray[i] = new Point(screenPoint.x, screenPoint.y, 0);
+        }
+
+        Gesture newGesture = new Gesture(pointArray);
+
+        if(creationMode)
+        {
+            newGesture.Name = newGestureName;
+            trainingSet.Add(newGesture);
+
+            string fileName = Application.persistentDataPath + "/" + newGestureName + ".xml";
+            GestureIO.WriteGesture(pointArray, newGestureName, fileName);
+        }
+        else
+        {
+            Result result = PointCloudRecognizer.Classify(newGesture, trainingSet.ToArray());
+            Debug.Log(result.GestureClass + result.Score);
+
+            if(result.Score > recognitionThreshold)
+            {
+                OnRecognized.Invoke(result.GestureClass);
+            }
+
+        }
+    }
+
+    void UpdateMovement()
+    {
+        Debug.Log("Update movement");
+
+        Vector3 lastPosition = positionList[positionList.Count - 1];
+
+        if (Vector3.Distance(movementSource.position, lastPosition) > newPositionThresholdDistance)
+        {
+            positionList.Add(movementSource.position);
+
+            if (drawingObjectPrefab)
+            {
+                Destroy(Instantiate(drawingObjectPrefab, movementSource.position, Quaternion.identity), 2f);
+            }
+        }
+    }
+}
