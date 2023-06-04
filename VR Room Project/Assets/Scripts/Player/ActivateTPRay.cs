b3d25d80f6d5315f1b62558415082096aeceb513
Dani-24 <79161140+Dani-24@users.noreply.github.com>
2023-06-04 15:01:20 +0200

    VR Player Functionality Settings

--- VR Room Project/Assets/Scripts/Player/ActivateTPRay.cs
+++ VR Room Project/Assets/Scripts/Player/ActivateTPRay.cs
@@ -11,9 +11,12 @@ public class ActivateTPRay : MonoBehaviour
     public InputActionProperty leftActivate;
     public InputActionProperty rightActivate;
 
+    public InputActionProperty leftCancel;
+    public InputActionProperty rightCancel;
+
     void Update()
     {
-        leftTP.SetActive(leftActivate.action.ReadValue<float>() > 0.1f);
-        rightTP.SetActive(rightActivate.action.ReadValue<float>() > 0.1f);
+        leftTP.SetActive(leftCancel.action.ReadValue<float>() == 0 && leftActivate.action.ReadValue<float>() > 0.1f);
+        rightTP.SetActive(rightCancel.action.ReadValue<float>() == 0 && rightActivate.action.ReadValue<float>() > 0.1f);
     }
 }
